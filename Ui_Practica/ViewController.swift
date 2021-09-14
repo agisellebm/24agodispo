2fe8049db93f6da6ab863dff1668a935116e9a67
Aileen <53828127+agisellebm@users.noreply.github.com>
2021-09-14 00:19:06 -0700

    Add files via upload

---
+++ Ui_Practica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ui_Practica
+//
+//  Created by Alumno on 8/24/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
